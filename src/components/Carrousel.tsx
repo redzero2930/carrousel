498ce2ad4a77d6c32b407c456a4a24eb2eecbd9b
Angel <redzero2930@gmail.com>
2023-08-22 16:55:54 -0700

    checkpoint

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,29 +1,31 @@
-// import {useState} from 'react'
+import {useState} from 'react'
+import xbox from '../assets/img/xbox.png'
 
 export default function Carrousel (props: any){
-    // const images = ['nintendo.png','playstation.jpg','xbox.png']
-    // const [selectedIndex, setSelectedIndex] = useState(0);
-    // const [selectedImage, setSelectedImage] = useState(images[0]);
+    const images = ['nintendo.png','playstation.jpg','xbox.png']
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedImage, setSelectedImage] = useState(images[0]);
 
-    // const selectNewImage = (index: number, images: string[], next = true) => {
-    //     const condition = next ? selectedIndex < images.length : selectedIndex > 0; 
-    //     const nextIndex = next ? (condition ? selectedIndex + 1 : 0) : condition ? selectedIndex - 1 : images.length - 1 
-    //     setSelectedImage(images[nextIndex])
-    //     setSelectedIndex(nextIndex)
-    // }
+    const selectNewImage = (index: number, images: string[], next = true) => {
+        const condition = next ? selectedIndex < images.length : selectedIndex > 0; 
+        const nextIndex = next ? (condition ? selectedIndex + 1 : 0) : condition ? selectedIndex - 1 : images.length - 1 
+        setSelectedImage(images[nextIndex])
+        setSelectedIndex(nextIndex)
+    }
 
-    // const previous = () => {
-    //     selectNewImage(selectedIndex, images, false)
-    // }
+    const previous = () => {
+        selectNewImage(selectedIndex, images, false)
+    }
 
-    // const next = () => {
-    //     selectNewImage(selectedIndex, images, false)
-    // }
+    const next = () => {
+        selectNewImage(selectedIndex, images, false)
+    }
 
     return(
         <>
-            <img src='../assets/img/nintendo.png' alt="console"/>
+            <img src={xbox} alt="console"/>
         </>
     )
 }
+
 
