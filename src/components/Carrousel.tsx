4137ec18497669f8bf5443d21f79d99edb14b44e
@redzero2930 <redzero2930@gmail.com>
2023-08-22 22:11:53 -0600

    checkpoint

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,31 +1,40 @@
-import {useState} from 'react'
-import xbox from '../assets/img/xbox.png'
+import { useState } from 'react';
+import nintendo from '../assets/img/nintendo.png';
+import playstation from '../assets/img/playstation.jpg';
+import xbox from '../assets/img/xbox.png';
+import './Carrousel.css'
 
-export default function Carrousel (props: any){
-    const images = ['nintendo.png','playstation.jpg','xbox.png']
+export default function Carrousel(props: any) {
+    const images = [nintendo, playstation, xbox];
     const [selectedIndex, setSelectedIndex] = useState(0);
     const [selectedImage, setSelectedImage] = useState(images[0]);
 
-    const selectNewImage = (index: number, images: string[], next = true) => {
-        const condition = next ? selectedIndex < images.length : selectedIndex > 0; 
-        const nextIndex = next ? (condition ? selectedIndex + 1 : 0) : condition ? selectedIndex - 1 : images.length - 1 
-        setSelectedImage(images[nextIndex])
-        setSelectedIndex(nextIndex)
-    }
+    const selectNewImage = (images: string[], next = true) => {
+        const condition = next ? selectedIndex < images.length - 1 : selectedIndex > 0;
+        const nextIndex = next ? (condition ? selectedIndex + 1 : 0) : condition ? selectedIndex - 1 : images.length - 1;
+        setSelectedImage(images[nextIndex]);
+        setSelectedIndex(nextIndex);
+    };
 
     const previous = () => {
-        selectNewImage(selectedIndex, images, false)
-    }
+        selectNewImage( images, false);
+    };
 
     const next = () => {
-        selectNewImage(selectedIndex, images, false)
-    }
+        selectNewImage( images, true);
+    };
 
-    return(
-        <>
-            <img src={xbox} alt="console"/>
-        </>
-    )
+    return (
+        <div className='carrousel'>
+            <img 
+            src={selectedImage} 
+            alt="console"
+            />
+            <button onClick={previous}>{"<"}</button>
+            <button onClick={next}>{">"}</button>
+        </div>
+    );
 }
+
 
 
